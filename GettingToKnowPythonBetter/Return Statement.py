42a79b8aaacf085e88ef5b1fc36005f446b9e1ab
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:45:13 +0200

    Added Return Statement.py file

---
+++ GettingToKnowPythonBetter/Return Statement.py
@@ -0,0 +1,11 @@
+print("RETURN STATEMENT!!!\n")
+
+
+def cube(num):
+    return num * num * num  # return a value
+
+
+print(cube(3))
+
+result = cube(3)  # will not print 'cube(3)' but the value 'returned' from function
+print(result)
