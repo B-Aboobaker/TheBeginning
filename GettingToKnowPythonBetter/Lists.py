f867403de5d1902b5200b67f8a65f4f087f3047e
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:43:26 +0200

    Added Lists.py file

---
+++ GettingToKnowPythonBetter/Lists.py
@@ -0,0 +1,10 @@
+print("LISTS!!!\n")
+
+friends = ["Kevin", "Kelly", "John", "Mike", "Samantha", "Nicky", 14, 36]
+
+friends[0] = "Aboobaker"
+
+print(len(friends))
+
+print(friends[0:4])
+print(friends[4:6])
