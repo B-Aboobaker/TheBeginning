bdb278933c32dd724870d6bcf9687622ba253bdd
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:03:07 +0200

    Added Variables & Data Types.py file

---
+++ PythonBasics/Variables & Data Types.py
@@ -0,0 +1,13 @@
+print("VARIABLES & DATA TYPES!!!\n")
+
+character_name = "George"  # this is a string type variable
+character_age = str(21)  # this is a integer type variable
+is_male = True  # this is a boolean type variable
+
+print("There once was a man named " + character_name + ".")
+print("He was " + character_age + " years old.")
+
+character_name = "Aboobaker"
+
+print("He really liked the name " + character_name + ".")
+print("But he didn't like being " + character_age + ".")
