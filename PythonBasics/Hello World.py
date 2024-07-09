a6a315c9ff4d424691c415c7d9bc4c88159cd9cd
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 10:32:04 +0200

    Added my first ever .py file

---
+++ PythonBasics/Hello World.py
@@ -0,0 +1,8 @@
+print("Hello World!!!\n")
+
+print("This is my first Python program.\n"
+      "Let's create a shape or two:\n\n"
+      "   /|    ______ \n"
+      "  / |   |      |\n"
+      " /  | + |      |\n"
+      "/___|   |______|\n")
