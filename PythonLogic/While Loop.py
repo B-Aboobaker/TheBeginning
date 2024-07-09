1b2147a7d8921a44ff34a27fd016a1a8f0cfd3e7
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:39:12 +0200

    Added While Loop.py file

---
+++ PythonLogic/While Loop.py
@@ -0,0 +1,17 @@
+
+print("WHILE LOOP!!!")
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 2
+
+
+print("")
+
+i = 0
+while i <= 150:
+    print(i)
+    i += 11  # change this number for timetables
+
