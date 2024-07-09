03a067d094341be72015a642cd3eb183b026dc5f
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:04:37 +0200

    Added Getting Input From Users.py file

---
+++ PythonBasics/Getting Input From Users.py
@@ -0,0 +1,6 @@
+print("GETTING INPUT FROM USERS!!!\n")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("\nWelcome " + name.capitalize() + "! You are " + age + " years old.")
