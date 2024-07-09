223b3f54425658106865876f44f4ec376e2eaee8
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:38:41 +0200

    Added if Statements & Comparisons.py file

---
+++ PythonLogic/if Statements & Comparisons.py
@@ -0,0 +1,13 @@
+print("if STATEMENTS & COMPARISONS\n")
+
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(14, 2, 70))
