c01e179f6ecded7ecb69c217a8c47a0bbbd612f5
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:01:11 +0200

    Added Numbers.py file

---
+++ PythonBasics/Numbers.py
@@ -0,0 +1,23 @@
+print("NUMBERS!!!\n")
+
+print(300.4 + 64.6)
+print("")
+
+equation = 3 + 4.5
+print(equation)
+print("")
+
+print(3 * (4 + 5))  # BODMAS works just fine
+print(10 % 3)  # 10 mod 3 will return remainder
+
+print("is equal to: " + str(equation))  # convert int/float to str when concatenating
+
+print(pow(4, 3))  # raise to the power
+print(max(325, 46))  # highest between two numbers
+print(round(42.2))  # standard round off
+
+from math import *  # import 'math' to access more math functions
+
+print(floor(22.9))  # round off to lowest
+print(ceil(22.2))  # round off to highest
+print(sqrt(36))  # square root
