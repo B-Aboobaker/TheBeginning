717023d07d46bcbdc40db3264a9f34d52d532920
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:00:06 +0200

    Added Strings.py file

---
+++ PythonBasics/Strings.py
@@ -0,0 +1,17 @@
+print("STRINGS!!!\n")
+
+phrase = "Aboobaker Banoo Brown"
+
+print(phrase + " is my full name and surname.\n")  # this is called concatenation
+
+#  name = input("Enter your name: ")
+#  print("Welcome " + name.capitalize() + "!\n")
+
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.isupper())
+print(len(phrase))
+print(phrase.index("ba"))
+print(phrase[0:10])
+print(phrase.upper().isupper())
+print(phrase.replace("baker", "zombie"))
