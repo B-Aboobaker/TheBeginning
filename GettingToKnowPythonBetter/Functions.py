41aa7d9b2b87599f806d72e2fcea25df1375072e
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:44:49 +0200

    Added Functions.py file

---
+++ GettingToKnowPythonBetter/Functions.py
@@ -0,0 +1,9 @@
+print("FUNCTIONS!!!\n")
+
+
+def say_hi(name, age):
+    print("Welcome " + name.capitalize() + "! You are " + str(age) + " years old.")
+
+
+say_hi("Aboobaker", 21)
+say_hi("Zeenat", 42)
