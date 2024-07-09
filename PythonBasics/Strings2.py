dcbdd2d710178b2d03bd43999b9e35452feea49c
Aboobaker Banoo <112656701+B-Aboobaker@users.noreply.github.com>
2024-07-09 11:02:01 +0200

    Added Strings2.py file

---
+++ PythonBasics/Strings2.py
@@ -0,0 +1,12 @@
+print("STRINGS!!!\n")
+
+visual_purpose = """Visual Purpose:\n
+A b o o b a k e r   B  a  n  o  o     B  r  o  w  n
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20\n"""
+
+print(visual_purpose)
+
+name = "Aboobaker Banoo Brown"
+
+print(name)
+print(name[0:21])
